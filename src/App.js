97402d520c9d6179040e5253fe278c0eb47c189c
dsthuyr <dinhsithuy@gmail.com>
2022-08-28 16:28:55 +0700

    Update: Add react lazy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import Layout from "./components/Layout/Layout";
-import AuthPage from "./pages/AuthPage";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ProfilePage from "./pages/ProfilePage";
+
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import axios from "axios";
 import { movieActions } from "./store/movies";
-import CategoryMoviePage from "./pages/CategoryMoviePage";
-import DetailPage from "./pages/DetailPage";
-import PageNotFound from "./pages/PageNotFound";
-import NotifyPage from "./pages/NotifyPage";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+// import CategoryMoviePage from "./pages/CategoryMoviePage";
+// import DetailPage from "./pages/DetailPage";
+// import PageNotFound from "./pages/PageNotFound";
+// import NotifyPage from "./pages/NotifyPage";
+// import AuthPage from "./pages/AuthPage";
+// import HomePage from "./pages/HomePage";
+// import ProfilePage from "./pages/ProfilePage";
+
+const CategoryMoviePage = React.lazy(() => import("./pages/CategoryMoviePage"));
+const DetailPage = React.lazy(() => import("./pages/DetailPage"));
+const PageNotFound = React.lazy(() => import("./pages/PageNotFound"));
+const NotifyPage = React.lazy(() => import("./pages/NotifyPage"));
+const AuthPage = React.lazy(() => import("./pages/AuthPage"));
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const ProfilePage = React.lazy(() => import("./pages/ProfilePage"));
 
 const theme = createTheme({
-  palette: {
-    custom1: {
-      main: "#333",
-    },
-    custom2: {
-      main: "#CE0000",
-    },
-  },
+  // palette: {
+  //   custom1: {
+  //     main: "#333",
+  //   },
+  //   custom2: {
+  //     main: "#CE0000",
+  //   },
+  // },
 });
 
 function App() {
@@ -44,25 +54,35 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/category/:categoryName"
-            element={<CategoryMoviePage />}
-          />
-          <Route path="/auth" element={<AuthPage />} />
-          {isLoggedIn && <Route path="/profile" element={<ProfilePage />} />}
-          {isLoggedIn && (
-            <Route path="/detailMovie/:movieId" element={<DetailPage />} />
-          )}
-          {!isLoggedIn && (
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<HomePage />} />
             <Route
-              path="/detailMovie/:movieId"
-              element={<NotifyPage text="Please login to use this feature!" />}
+              path="/category/:categoryName"
+              element={<CategoryMoviePage />}
             />
-          )}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+            <Route path="/auth" element={<AuthPage />} />
+            {isLoggedIn && <Route path="/profile" element={<ProfilePage />} />}
+            {isLoggedIn && (
+              <Route path="/detailMovie/:movieId" element={<DetailPage />} />
+            )}
+            {!isLoggedIn && (
+              <Route
+                path="/detailMovie/:movieId"
+                element={
+                  <NotifyPage text="Please login to use this feature!" />
+                }
+              />
+            )}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </ThemeProvider>
   );
